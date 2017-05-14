37694b63d928036c848fb0c2b02f1e1db160f6e3
Ranjith <ranjithstar256@gmail.com>
2017-05-14 13:31:46 +0530

    Update StartScreen.java
    i have made few changes
    there's more changes are there.. i will tell you clearly in todays class :)

--- Exercises1-ListViews/app/src/main/java/kp/ranjith/helloapp/StartScreen.java
+++ Exercises1-ListViews/app/src/main/java/kp/ranjith/helloapp/StartScreen.java
@@ -8,19 +8,29 @@ import android.widget.Toast;
 
 public class StartScreen extends AppCompatActivity {
 
+    Button radio1,radio2, radio3;   // always follow 
+    //1.Declaration above oncreate
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        
+        
+        radio1 = (Button) findViewById(R.id.radioButton); //2.initialization inside oncreate
+        radio2 = (Button) findViewById(R.id.radioButton);
+        radio3 = (Button) findViewById(R.id.radioButton); // its recommended to initialize the variables inside oncreate method
 
     }
 
-    public void onListViewClick(){
-        Button radio1 = (Button) findViewById(R.id.radioButton);
-        Button radio2 = (Button) findViewById(R.id.radioButton);
-        Button radio3 = (Button) findViewById(R.id.radioButton);
+    public void onListViewClick(View v){
 
         Intent startIntent = null;
+        
+        //id = v.getId();
+        // we have to use switch here.. // recommended 
+        
+        // i think there's more changes are there.. i will tell you clearly in todays class :)
 
         if (radio1.isSelected()){
             startIntent =  new Intent(StartScreen.this, ListViewActivity.class);
